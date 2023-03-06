6aeefc141ff5118c616bd7f63f55b89af75abcc8
Artur Wrona <arturwrona91@gmail.com>
2023-03-06 11:01:02 +0100

    Rewrite functional

--- addons/ofxThinkgear/src/ThinkgearCommsDriver.h
+++ addons/ofxThinkgear/src/ThinkgearCommsDriver.h
@@ -2,13 +2,7 @@
 
 #include "ofMain.h"
 
-
-#ifdef _WIN32
-#include <functional> // tr1 deprecated in VS
-#else
-#include <tr1/functional>
-#endif
-
+#include <functional>
 
 /*
  ThinkgearCommsDriver.
@@ -73,7 +67,7 @@ public:
     ~ThinkgearCommsDriver();
 
     bool isReady;
-    std::tr1::function<void(int,float)> callback;
+    std::function<void(int,float)> callback;
     string deviceName;
     int baudRate;
     
@@ -81,7 +75,7 @@ public:
     void setup(string deviceName, int baudRate,T  * listener, void (T::*listenerMethod)(int,float)) {
         this->deviceName = deviceName;
         this->baudRate = baudRate;
-        callback = std::tr1::bind(listenerMethod, listener, std::tr1::placeholders::_1, std::tr1::placeholders::_2);
+        callback = std::bind(listenerMethod, listener, std::placeholders::_1, std::placeholders::_2);
     };
     
     bool connect();
